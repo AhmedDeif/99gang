391125c1e2900cb92c82c9aa012916e10fa23f41
Doola <ahmed.adel.abodeif@gmail.com>
2019-04-21 15:13:18 +0200

    setting textfield delegate

--- 99gang/InteractiveTextfieldView.swift
+++ 99gang/InteractiveTextfieldView.swift
@@ -38,6 +38,31 @@ class InteractiveTextfieldView: UIView {
     
     func viewSetup() {
         textfield.applyCustomClearButton()
+        textfield.delegate = self
     }
+    
+    
+}
+
+extension InteractiveTextfieldView: UITextFieldDelegate {
+    
+    
+    func textFieldShouldBeginEditing(_ textField: UITextField) -> Bool {
+        DispatchQueue.main.async {
+            UIView.animate(withDuration: 0.2, animations: {
+                self.borderView.backgroundColor = UIColor(red: 1, green: 1, blue: 1, alpha: 1)
+            })
+        }
+        return true
+    }
+    
+    func textFieldShouldEndEditing(_ textField: UITextField) -> Bool {
+        DispatchQueue.main.async {
+            self.borderView.backgroundColor = UIColor(red: 1, green: 1, blue: 1, alpha: 0.3)
+        }
+        return true
+    }
+
+    
 }
 
