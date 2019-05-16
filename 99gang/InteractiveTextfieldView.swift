47c5e81595a3ad6d4230e6545ca5fb22ac78b328
Doola <ahmed.adel.abodeif@gmail.com>
2019-05-16 15:57:35 +0200

    adding network manager

--- 99gang/InteractiveTextfieldView.swift
+++ 99gang/InteractiveTextfieldView.swift
@@ -10,18 +10,30 @@ import UIKit
 
 class InteractiveTextfieldView: UIView {
     
-    let nibName = "InteractiveTextfieldView"
     
+    let nibName = "InteractiveTextfieldView"
     @IBOutlet weak var textfield: UITextField!
     @IBOutlet weak var borderView: UIView!
     @IBOutlet var contentView: UIView!
     @IBOutlet weak var validationLabel: UILabel!
+    
+    var textfieldType: fieldType?
+    var shouldFinishEditingCompletion: (()->())?
+    
+    
+    enum fieldType {
+        case email
+        case password
+        case numeric
+        case other
+    }
     
     
     override init(frame: CGRect) {
         super.init(frame: frame)
         commonInit()
     }
+    
     
     required init?(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)
@@ -36,16 +48,63 @@ class InteractiveTextfieldView: UIView {
         viewSetup()
     }
     
+    
     func viewSetup() {
         textfield.applyCustomClearButton()
         textfield.delegate = self
+        
+        // Params
+        if let fieldType = self.textfieldType {
+            switch fieldType {
+            case .email:
+                print("email textfield")
+                textfield.keyboardType = .emailAddress
+                break
+            case .password:
+                print("password textfield")
+                textfield.isSecureTextEntry = true
+                break
+            case .numeric:
+                textfield.keyboardType = .numberPad
+                break
+            case .other:
+                print("other textfield")
+            }
+        }
     }
     
     
+    func validateField() {
+        if let fieldType = self.textfieldType {
+            switch fieldType {
+            case .email:
+                validateEmailField()
+                break
+            case .password:
+                validatePassword()
+                break
+            case .numeric:
+                break
+            case .other:
+                print("other textfield")
+            }
+        }
+    }
+    
+    
+    func validateEmailField() {
+        validationLabel.text = "Incorrect email. Please try again."
+    }
+    
+    func validatePassword() {
+        validationLabel.text = "Incorrect Password. Please try again."
+    }
+    
 }
 
+
+
 extension InteractiveTextfieldView: UITextFieldDelegate {
-    
     
     func textFieldShouldBeginEditing(_ textField: UITextField) -> Bool {
         DispatchQueue.main.async {
@@ -57,12 +116,15 @@ extension InteractiveTextfieldView: UITextFieldDelegate {
     }
     
     func textFieldShouldEndEditing(_ textField: UITextField) -> Bool {
+        validateField()
         DispatchQueue.main.async {
-            self.borderView.backgroundColor = UIColor(red: 1, green: 1, blue: 1, alpha: 0.3)
+            UIView.animate(withDuration: 0.2, animations: {
+                self.borderView.backgroundColor = UIColor(red: 1, green: 1, blue: 1, alpha: 0.3)
+            })
         }
+        self.shouldFinishEditingCompletion?()
         return true
     }
-
     
 }
 
